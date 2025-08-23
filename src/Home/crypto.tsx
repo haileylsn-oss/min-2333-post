a4fe60be1ca5032b53fc721820e3c7c4cace2d27
haileylsn-oss <haileylsn@gmail.com>
2025-08-23 22:46:14 +0100

    pohh

--- src/Home/crypto.tsx
+++ src/Home/crypto.tsx
@@ -3,17 +3,17 @@
 export default function CryptoMarketScreener() {
   return (
     <div className="w-full max-w-6xl mx-auto py-10">
-      <div className="w-full h-[450px]">
-        <iframe
-          title="crypto-mkt-screener"
-          lang="en"
-          frameBorder="0"
-          allowTransparency={true}
-          scrolling="no"
-          src="https://www.tradingview-widget.com/embed-widget/crypto-mkt-screener/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A450%2C%22defaultColumn%22%3A%22overview%22%2C%22screener_type%22%3A%22crypto_mkt%22%2C%22displayCurrency%22%3A%22GBP%22%2C%22colorTheme%22%3A%22light%22%2C%22market%22%3A%22crypto%22%2C%22enableScrolling%22%3Atrue%7D"
-          style={{ width: "100%", height: "100%", display: "block" }}
-        ></iframe>
-      </div>
+   <div className="w-full h-[450px]">
+  <iframe
+    title="crypto-mkt-screener"
+    lang="en"
+    // scrolling="no"
+    allowTransparency
+    src="https://www.tradingview-widget.com/embed-widget/crypto-mkt-screener/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A450%2C%22defaultColumn%22%3A%22overview%22%2C%22screener_type%22%3A%22crypto_mkt%22%2C%22displayCurrency%22%3A%22GBP%22%2C%22colorTheme%22%3A%22light%22%2C%22market%22%3A%22crypto%22%2C%22enableScrolling%22%3Atrue%7D"
+    style={{ width: "100%", height: "100%", display: "block", border: "none" }}
+  ></iframe>
+</div>
+
 
       <div className="text-center text-gray-400 text-sm mt-2">
         <a
--- src/components/forex.tsx
+++ src/components/forex.tsx
@@ -4,26 +4,25 @@ export default function ForexCrossRatesWidget() {
   return (
     <section className="px-6 py-10 bg-gray-50">
       <div className="max-w-6xl mx-auto">
-        <div className="bg-white rounded-2xl shadow border overflow-hidden">
-          <iframe
-            title="forex cross-rates TradingView widget"
-            src="https://www.tradingview-widget.com/embed-widget/forex-cross-rates/?locale=en#%7B%22currencies%22%3A%5B%22EUR%22%2C%22USD%22%2C%22JPY%22%2C%22BTC%22%2C%22ETH%22%2C%22LTC%22%2C%22GBP%22%2C%22CHF%22%2C%22AUD%22%2C%22CAD%22%2C%22NZD%22%2C%22CNY%22%5D%2C%22colorTheme%22%3A%22light%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A600%2C%22utm_source%22%3A%22truenorthassetspartners.org%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22forex-cross-rates%22%2C%22page-uri%22%3A%22truenorthassetspartners.org%2F%22%7D"
-            frameBorder="0"
-            scrolling="no"
-            style={{ display: "block", width: "100%", height: "600px" }}
-          />
-          <div className="text-center text-sm text-gray-400 py-3">
-            <a
-              href="https://www.tradingview.com/markets/currencies/forex-cross-rates/"
-              rel="noopener"
-              target="_blank"
-              className="text-blue-600 hover:underline"
-            >
-              Forex Rates
-            </a>{" "}
-            by TradingView
-          </div>
-        </div>
+     <div className="bg-white rounded-2xl shadow border overflow-hidden">
+  <iframe
+    title="forex cross-rates TradingView widget"
+    src="https://www.tradingview-widget.com/embed-widget/forex-cross-rates/?locale=en#%7B%22currencies%22%3A%5B%22EUR%22%2C%22USD%22%2C%22JPY%22%2C%22BTC%22%2C%22ETH%22%2C%22LTC%22%2C%22GBP%22%2C%22CHF%22%2C%22AUD%22%2C%22CAD%22%2C%22NZD%22%2C%22CNY%22%5D%2C%22colorTheme%22%3A%22light%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A600%2C%22utm_source%22%3A%22truenorthassetspartners.org%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22forex-cross-rates%22%2C%22page-uri%22%3A%22truenorthassetspartners.org%2F%22%7D"
+    style={{ display: "block", width: "100%", height: "600px", border: "none", overflow: "hidden" }}
+  />
+  <div className="text-center text-sm text-gray-400 py-3">
+    <a
+      href="https://www.tradingview.com/markets/currencies/forex-cross-rates/"
+      rel="noopener noreferrer"
+      target="_blank"
+      className="text-blue-600 hover:underline"
+    >
+      Forex Rates
+    </a>{" "}
+    by TradingView
+  </div>
+</div>
+
       </div>
     </section>
   );
